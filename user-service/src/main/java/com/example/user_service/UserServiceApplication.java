f993786fc07891661846fee1a75540144b999d9e
Aman Vishwakarma <amanvishwakarma922@gmail.com>
2025-09-25 16:36:13 +0530

    Tomcate Implementation is done

--- user-service/src/main/java/com/example/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/example/user_service/UserServiceApplication.java
@@ -3,11 +3,16 @@ package com.example.user_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
+
+    }
 	}
 
-}
+
